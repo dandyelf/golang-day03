9a0398ae0f74361c43ef55ccbcf0e92a42691d76
leftrana <dandyelf32@gmail.com>
2024-02-29 19:20:31 +0300

    search func add

--- src/ex00/elastic_start.sh
+++ src/ex00/elastic_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/opt/goinfre/leftrana/elasticsearch-8.4.2/bin/elasticsearch -E xpack.security.enabled=false
+/opt/goinfre/leftrana/elasticsearch-8.4.2/bin/elasticsearch
+#  -E xpack.security.enabled=false
--- src/ex00/main.go
+++ src/ex00/main.go
@@ -1,12 +1,47 @@
 package main
 
 import (
-	"github.com/elastic/go-elasticsearch/v8"
+	"fmt"
 	"log"
+	"strings"
+
+	"github.com/elastic/go-elasticsearch/v8"
 )
 
 func main() {
-	es, _ := elasticsearch.NewDefaultClient()
+	err, esCfg := userInterface()
+	if err != nil {
+		fmt.Println("Crit error")
+		return
+	}
+	esClient, err := elasticsearch.NewClient(esCfg)
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+	// Check connection
+	code, err := esClient.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging the Elasticsearch server: %s", err)
+	}
+	log.Println("Elasticsearch returned with code___", code.Status())
 	log.Println(elasticsearch.Version)
-	log.Println(es.Info())
+	log.Println(esClient.Info())
+
+	query := `{ "query": { "match_all": {} } }`
+	result, err := esClient.Search(
+		esClient.Search.WithIndex("my_index"),
+		esClient.Search.WithBody(strings.NewReader(query)),
+	)
+	fmt.Println("Search executed successfully", err, result.IsError(), result.String())
+}
+
+func userInterface() (err error, cfg elasticsearch.Config) {
+	cfg = elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+		Username: "elastic",
+		Password: "changeme",
+	}
+	return
 }
